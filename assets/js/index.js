eb00234ec0420ec9ad67d8d78145ab15244ba284
Jerry Hu <work.jerrywu@gmail.com>
2024-02-12 13:56:18 -0800

    added connection to server

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,18 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     var tryButton = document.getElementById('tryit-btn');
     var tryitForm = document.getElementById('tryit-form');
 
-    tryButton.addEventListener('click', function () {
-        tryButton.style.display = 'none'; // Hide the tryButton
-        tryitForm.style.display = 'block'; // Show the tryit-form content
-    });
+    // tryButton.addEventListener('click', function () {
+    //     tryButton.style.display = 'none'; // Hide the tryButton
+    //     tryitForm.style.display = 'block'; // Show the tryit-form content
+    // });
 
     var url = new URL(window.location.href);
     var searchParams = new URLSearchParams(url.search);
     var boolValue = searchParams.get('tryit');
     var shouldTry = boolValue === 'true';
     if (shouldTry) {
-        tryButton.style.display = 'none'; // Hide the tryButton
-        tryitForm.style.display = 'block'; // Show the tryit-form content
+        // tryButton.style.display = 'none'; // Hide the tryButton
+        // tryitForm.style.display = 'block'; // Show the tryit-form content
+        
+        window.location = 'http://127.0.0.1:9999'
     };
 
     firebase.auth().onAuthStateChanged(function (user) {
